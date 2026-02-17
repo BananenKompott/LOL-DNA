88006e240886cd6c0c130459a51fb515118aac40
Zechi <1206@htl.rennweg.at>
2026-02-17 15:33:04 +0100

    Initial commit

---
+++ src/main/java/com/example/loldna/LolDnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loldna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolDnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolDnaApplication.class, args);
+    }
+
+}
