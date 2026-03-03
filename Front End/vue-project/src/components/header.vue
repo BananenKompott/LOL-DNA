42a2e4c3164dbafa34c6ec012ccab188b34fe230
schneckifyyy <adamogris@outlook.at>
2026-03-03 17:45:14 +0100

    Frontend start

---
+++ Front End/vue-project/src/components/header.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>LOL DNA</h1>
+    Search for your favorite Player on EU WEST Server! (DKB NIQKL)
+  </header>
+</template>
+
+<style scoped>
+
+</style>
